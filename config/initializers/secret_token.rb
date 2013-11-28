5f74afc74af0c0c217491ee75eb8fd4789802093
rwwaskk <tsubomi1989@gmail.com>
2013-11-28 09:38:22 -0800

    all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreshCoastSoccer::Application.config.secret_token = '57d83e3b4e37e2b7de0f060940acebb49369358c1dad04f6b63949eb030ab22ae58a19a9129fffd19c3951bcd31c3f63fb148220b4aae112d630113be403c71b'
---
+++ config/routes.rb
@@ -0,0 +1,70 @@
+FreshCoastSoccer::Application.routes.draw do
+  
+  root :to => 'home#index'
+  
+  #static pages
+  match '/announce' => 'home#announce'
+  match '/registration' => 'home#registration'
+  match '/registration_forms' => 'home#registration_forms'
+  match '/invite' => 'home#invite'
+  match '/sponsors' => 'home#sponsors'
+  match '/contact' => 'home#contact'
+  match '/champs' => 'home#champs'
+  match '/pro_store' => 'home#pro_store'
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => 'welcome#index'
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id))(.:format)'
+end
