668c4f5d353dbee7dc3c64499ca4d013dddb4901
rwwaskk <tsubomi1989@gmail.com>
2013-12-10 16:34:21 -0800

    action mailer

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -2,7 +2,9 @@ class InvitationMailer < ActionMailer::Base
   default from: "info@freshcoastbeachsoccer.com"
   def invite(invitation)
     @invitation=invitation
+    attachments.inline['flyer_email(new).jpg'] = File.read("#{Rails.root}/public/flyer_email(new).jpg")
     mail(to:@invitation.receiver_email,subject: 'Interested In Beach Soccer?')
+    
   end
   
 end
